d37b946424522fe9042da974aee80b240eae0903
assomatic <90207584+assomatic@users.noreply.github.com>
2021-11-09 18:44:26 +0100

    serverless_f_1

---
+++ examples/1-hello/app.js
@@ -0,0 +1,12 @@
+const result = document.querySelector('.result');
+
+const fetchData = async () => {
+  try {
+    const { data } = await axios.get('api/1-hello'); // redirect (api) siehe netlify.toml
+    result.textContent = data;
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+fetchData();
---
+++ functions/1-hello.js
@@ -0,0 +1,28 @@
+// domain/.netlify/functions/1-hello
+
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: 'Our First Netlify Function Example',
+  };
+};
+
+// mit callback als dritten Parameter aber ohne async
+
+// exports.handler = (event, context, callback) => {
+//   callback(null, {
+//     statusCode: 200,
+//     body: 'Our  second First Netlify Function Example',
+//   });
+// };
+
+// Objekte (oder Arrays) müssen stringified werden - sonst error
+
+// const person = { name: 'john' };
+// exports.handler = async (event, context) => {
+//   console.log(event, context);
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(person),
+//   };
+// };

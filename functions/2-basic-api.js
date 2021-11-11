3092b8b38150d556cbc2117cc9ce214b3b769ef1
assomatic <90207584+assomatic@users.noreply.github.com>
2021-11-11 17:45:34 +0100

    headers ...

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,9 +2,10 @@ const items = require('../assets/data.js');
 
 exports.handler = async (event, context) => {
   return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+    // Zugriff nur von 'innen' erlaubt:
+    // headers: {
+    //   'Access-Control-Allow-Origin': '*',
+    // },
     statusCode: 200,
     body: JSON.stringify(items),
   };
